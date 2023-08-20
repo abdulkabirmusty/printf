17d9fe978b771170a05b520cdddc108220590387
abdulkabirmusty <abdulkabir.abdulwasiu2020@nda.edu.ng>
2023-08-20 13:06:25 -0700

    Task 0

--- _printf.c
+++ _printf.c
@@ -6,9 +6,7 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i;
-	unsigned int count;
-	unsigned int string_count;
+	unsigned int i, string_count, count = 0;
 
 	va_list args;
 
@@ -18,32 +16,32 @@ int _printf(const char *format, ...)
 	{
 		switch (format[i])
 		{
-		case '%':
-		switch (format[i + 1])
-		{
-		case 'c':
-			_printchar(va_arg(args, int));
-			i++;
-			break;
-		case 's':
-			string_count = _printstring(va_arg(args, char *));
-			i++;
-			count += (string_count - 1);
-			break;
-		case '%':
-			_printchar('%');
-			i++;
-			break;
-		default:
-			_printchar('%');
-			break;
-		}
+			case '%':
+			switch (format[i + 1])
+			{
+			case 'c':
+				_printchar(va_arg(args, int));
+				i++;
+				break;
+			case 's':
+				string_count = _printstring(va_arg(args, char *));
+				i++;
+				count += (string_count - 1);
+				break;
+			case '%':
+				_printchar('%');
+				i++;
+				break;
+			default:
+				count++;
+				break;
+			}
 		break;
 		default:
-		_printchar(format[i]);
-		break;
+			_printchar(format[i]);
+			break;
 		}
-	count++;
+		count++;
 	}
 	va_end(args);
 	return (count);
