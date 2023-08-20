0597852982950d11b24fdcf08f9408bebc5aa520
KoderKareem <abdulkarimsunusi2001@gmail.com>
2023-08-20 15:54:14 -0700

    make some on swtch stm

--- _printf.c
+++ _printf.c
@@ -2,41 +2,48 @@
 /**
  * _printf - output text
  * @format: the format specifier
- * Return: (COUNT);
+ * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i, string_count, count = 0;
-
+	unsigned int i;
+	unsigned int count;
+	unsigned int string_count;
 	va_list args;
-
-	if (!format || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
 
 	va_start(args, format);
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] != '%')
+		if (format[i] == '%')
 		{
+		i++;
+		switch (format[i])
+		{
+		case 'c':
+			_printchar(va_arg(args, int));
+			count++;
+			break;
+		case 's':
+			string_count = _printstring(va_arg(args, char *));
+			count += string_count;
+			break;
+		case '%':
+			_printchar('%');
+			count++;
+			break;
+		default:
+			_printchar('%');
+			_printchar(format[i]);
+			count += 2;
+			break;
+			}
+		}
+		else
+		{		
 			_printchar(format[i]);
 		}
-		else if (format[i + 1] == 'c')
-		{
-			_printchar(va_arg(args, int));
-			i++;
-		}
-		else if (format[i + 1] == 's')
-		{
-			string_count = _printstring(va_arg(args, char *));
-			i++;
-			count += (string_count - 1);
-		}
-		else if (format[i + 1] == '%')
-		{
-			_printchar('%');
-		}
-		count += 1;
+		count++;
 	}
 	va_end(args);
 	return (count);
