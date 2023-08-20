400c797178f3824cae09f943c9745c6c0d739f9c
abdulkabirmusty <abdulkabir.abdulwasiu2020@nda.edu.ng>
2023-08-20 14:30:32 -0700

    Task 0 update

--- _printf.c
+++ _printf.c
@@ -10,38 +10,33 @@ int _printf(const char *format, ...)
 
 	va_list args;
 
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
 	va_start(args, format);
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		switch (format[i])
+		if (format[i] != '%')
 		{
-			case '%':
-			switch (format[i + 1])
-			{
-			case 'c':
-				_printchar(va_arg(args, int));
-				i++;
-				break;
-			case 's':
-				string_count = _printstring(va_arg(args, char *));
-				i++;
-				count += (string_count - 1);
-				break;
-			case '%':
-				_printchar('%');
-				i++;
-				break;
-			default:
-				count++;
-				break;
-			}
-		break;
-		default:
 			_printchar(format[i]);
-			break;
 		}
-		count++;
+		else if (format[i + 1] == 'c')
+		{
+			_printchar(va_arg(args, int));
+			i++;
+		}
+		else if (format[i + 1] == 's')
+		{
+			string_count = _printstring(va_arg(args, char *));
+			i++;
+			count += (string_count - 1);
+		}
+		else if (format[i + 1] == '%')
+		{
+			_printchar('%');
+		}
+		count += 1;
 	}
 	va_end(args);
 	return (count);
