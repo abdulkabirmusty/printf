e450b93423c3438bfd5379cce958da9ac547ab96
KoderKareem <abdulkarimsunusi2001@gmail.com>
2023-08-21 07:24:06 -0700

    make an update

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _printf - output text
  * @format: the format specifier
@@ -6,42 +7,44 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i;
-	unsigned int count;
-	unsigned int string_count;
+	unsigned int i, count = 0, string_count;
 	va_list args;
 
 	va_start(args, format);
 
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0; format && format[i]; i++)
 	{
-		if (format[i] == '%')
+		if (format[i] == '%' && format[i + 1])
 		{
-		i++;
-		switch (format[i])
-		{
-		case 'c':
-			_printchar(va_arg(args, int));
-			count++;
-			break;
-		case 's':
-			string_count = _printstring(va_arg(args, char *));
-			count += string_count;
-			break;
-		case '%':
-			_printchar('%');
-			count++;
-			break;
-		default:
-			_printchar('%');
-			_printchar(format[i]);
-			count += 2;
-			break;
+			i++;
+			if (format[i] == 'c')
+			{
+				_printchar(va_arg(args, int));
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				string_count = _printstring(va_arg(args, char *));
+				if (string_count == -1)
+					return (-1);
+				count += string_count;
+			}
+			else if (format[i] == '%')
+			{
+				_printchar('%');
+				count++;
+			}
+			else
+			{
+				_printchar('%');
+				_printchar(format[i]);
+				count += 2;
 			}
 		}
 		else
-		{		
+		{
 			_printchar(format[i]);
+			count++;
 		}
 	}
 	va_end(args);
