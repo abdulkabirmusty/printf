8c524ec6060f5ef14b5344f90da3afe9c4cc22d8
KoderKareem <abdulkarimsunusi2001@gmail.com>
2023-08-21 08:46:14 -0700

    update task 0

--- _printf.c
+++ _printf.c
@@ -2,37 +2,43 @@
 
 int _printf(const char *format, ...)
 {
-	unsigned int i, count = 0, string_count = 0;
-	
-	va_list args;
-	
-        if (!format || (format[0] == '%' && format[1] == '\0')) 
+    unsigned int i, count = 0, string_count = 0;
+    va_list args;
+      
+      if (!format || (format[0] == '%' && format[1] == '\0')) 
                  return (-1);
-	va_start(args, format);
-	
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
+    va_start(args, format);
+
+    for (i = 0; format[i] != '\0'; i++)
     {
-      _printchar(format[i]);
+        if (format[i] != '%')
+        {
+            _printchar(format[i]);
+            count++;
+        }
+        else
+        {
+            i++;
+            switch (format[i])
+            {
+                case 'c':
+                    _printchar(va_arg(args, int));
+                    break;
+                case 's':
+                    string_count = _printstring(va_arg(args, char *));
+                    count += string_count;
+                    break;
+                case '%':
+                    _printchar('%');
+                    break;
+                default:
+                    _printchar('%');
+                    _printchar(format[i]);
+                    break;
+            }
+            count++;
+        }
     }
-    else if (format[i] == '%' && format[i + 1] == 'c')
-    {
-      _printchar(va_arg(args, int));
-      i++;
-    }
-    else if (format[i] == '%' && format[i + 1] == 's')
-    {
-     string_count = _printstring (va_arg(args, char*));
-      i++;
-      count =+ (string_count - 1);
-    }
-    else if (format[i + 1] == '%')
-    {
-      _printchar('%');
-    }
-    count += 1;
-	}
-  va_end(args);
-  return (count);
+    va_end(args);
+    return (count);
 }
