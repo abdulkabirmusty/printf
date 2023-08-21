48bac8d9a0ce07227241a70d40620b1fbc8bb1bc
KoderKareem <abdulkarimsunusi2001@gmail.com>
2023-08-21 08:54:54 -0700

    update

--- _printf.c
+++ _printf.c
@@ -2,48 +2,44 @@
 
 int _printf(const char *format, ...)
 {
-    unsigned int i, count = 0, string_count = 0;
-    va_list args;
-      
+	unsigned int i, count = 0, string_count = 0;
+	va_list args;
+  
       if (!format || (format[0] == '%' && format[1] == '\0')) 
                  return (-1);
-    va_start(args, format);
+	va_start(args, format);
 
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] != '%')
-        {
-            _printchar(format[i]);
-            count++;
-        }
-        else
-        {
-            i++;
-            switch (format[i])
-            {
-                case 'c':
-                    _printchar(va_arg(args, int));
-                    count++;
-                    break;
-                case 's':
-                    string_count = _printstring(va_arg(args, char *));
-                    count += string_count;
-                  count++;
-                    break;
-                case '%':
-                    _printchar('%');
-                    count++;
-                    break;
-                default:
-                    _printchar('%');
-                    _printchar(format[i]);
-                    count++;
-                    break;
-            }
-            
-        }
-    }
-    va_end(args);
-    return (count);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		switch (format[i])
+		{
+			case '%':
+				switch (format[i + 1])
+				{
+					case 'c':
+						_printchar(va_arg(args, int));
+						i++;
+						break;
+					case 's':
+						string_count = _printstring(va_arg(args, char *));
+						i++;
+						count += (string_count - 1);
+						break;
+					case '%':
+						_printchar('%');
+						i++;
+						break;
+					default:
+						_printchar('%');
+						break;
+				}
+				break;
+			default:
+				_printchar(format[i]);
+				break;
+		}
+		count++;
+	}
+	va_end(args);
+	return (count);
 }
-
