5a9e5a32471106e5d3881509c6f30ce4b1338dc9
abdulkabirmusty <abdulkabir.abdulwasiu2020@nda.edu.ng>
2023-08-21 13:57:00 -0700

    Task 0 update

--- _printf.c
+++ _printf.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
+/**
+ * _printf - This produces output according to a format
+ * @format: The format is a character  string
+ *
+ * Return: Number of character printed
+ */
 int _printf(const char *format, ...)
 {
 	unsigned int i, count = 0, string_count = 0;
 	va_list args;
-  
-  if (!format || (format[0] == '%' && format[1] == '\0')) 
-                 return (-1);
+
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 	va_start(args, format);
 
 	for (i = 0; format[i] != '\0'; i++)
